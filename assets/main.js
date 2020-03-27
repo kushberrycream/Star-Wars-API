60d6f4310c81c2c263d0ad70dbde44887e0b79e0
kushberrycream <tom@wilson-express.co.uk>
2020-03-27 02:54:41 +0000

    added next buttons

--- assets/main.js
+++ assets/main.js
@@ -13,18 +13,19 @@ row.setAttribute("id", "data");
 const logo = document.createElement("img");
 logo.src = "assets/starwars.png";
 logo.setAttribute("class", "img-fluid");
+logo.setAttribute("alt", "StarWars Logo");
 
 const apiUrl = `https://swapi.co/api/`;
 
 const peopleBtn = document.createElement("button");
 peopleBtn.setAttribute("class", "btn btn-warning");
-peopleBtn.setAttribute("onclick", "doSomething(value)");
+peopleBtn.setAttribute("onclick", "doSomething(value) ");
 peopleBtn.innerText = "People";
 peopleBtn.value = "people";
 
 const planetsBtn = document.createElement("button");
 planetsBtn.setAttribute("class", "btn btn-warning");
-planetsBtn.setAttribute("onclick", "doSomething(value)");
+planetsBtn.setAttribute("onclick", "doSomething(value) ");
 planetsBtn.innerText = "Planets";
 planetsBtn.value = "planets";
 
@@ -64,30 +65,61 @@ app.appendChild(container);
 container.appendChild(row);
 
 let btnValue = {};
+let nextPage = {};
+let prevPage = {};
+
+function generatePaginationButtons(next, prev) {
+  next = next.slice(21, 35);
+  nextPage = next;
+  
+  if (!prev) {
+      
+  } else {
+      prev = prev.slice(21,35);
+      prevPage = prev
+  }
+  
+
+  if (next && prev) {
+    return `<div id="prev"><button onclick="doSomething(value)" value="${prevPage}">Previous</button></div>
+               <div id="next"><button onclick="doSomething(value)" value="${next}"><i class="fas fa-jedi rotate-right hvr-pulse-grow"></button></div>`;
+  } else if (next && !prev) {
+    return `<div id="next"><button onclick="doSomething(value)" value="${nextPage}"><i class="fas fa-jedi rotate-right hvr-pulse-grow"></i></i></button></div>`;
+  } else if (!next && prev) {
+    return `<div id="prev"><button onclick="doSomething(value)" value="${prev}>Previous</button></div>`;
+  }
+}
 
 function getInfo() {
   axios.get(apiUrl + btnValue).then(response => {
     this.data = response.data;
-    this.data.results.forEach(item => {
-      if (item.name) {
-        const card = document.createElement("div");
-        row.appendChild(card);
-        card.setAttribute("class", "card col-lg-3");
+    let pagination = "";
+    let nextPrev = document.createElement("div");
+    if (data.next || data.previous) {
+      pagination = generatePaginationButtons(data.next, data.previous);
+      row.appendChild(nextPrev);
+      nextPrev.setAttribute("class", "nextprev");
+      nextPrev.setAttribute("id", "nextprev");
+      nextPrev.innerHTML = `${pagination}`;
+      
+    }
+    data.results.forEach(item => {
+      let card = document.createElement("div");
+      row.appendChild(card);
+      card.setAttribute("class", "card col-lg-3");
 
-        card.innerHTML = `<h1>${item.name}</h1><p>Height: ${item.height}</p><br><p>Hair color: ${item.hair_color}</p><br><p>Birth Year: ${item.birth_year}</p><br>`
-
-        
-        
-      } else {
-        const card = document.createElement("div");
-        card.setAttribute("class", "card col-lg-3");
-
-        const h1 = document.createElement("h1");
-        h1.textContent = item.title;
-
-        row.appendChild(card);
-        card.appendChild(h1);
-    
+      if (item.height) {
+        card.innerHTML = `<h1>${item.name}</h1><h6>Height:</h6><p> ${item.height} cm</p><h6>Hair color:</h6><p>${item.hair_color}</p><h6>Birth Year:</h6><p>${item.birth_year}</p>`;
+      } else if (item.terrain) {
+        card.innerHTML = `<h1>${item.name}</h1><h6>Terrain:</h6><p> ${item.terrain}</p><h6>Gravity:</h6><p>${item.gravity}</p><h6>Climate:</h6><p>${item.climate}</p>`;
+      } else if (item.classification) {
+        card.innerHTML = `<h1>${item.name}</h1><h6>Classification:</h6><p> ${item.classification}</p><h6>Designation:</h6><p>${item.designation}</p><h6>Language:</h6><p>${item.language}</p>`;
+      } else if (item.vehicle_class) {
+        card.innerHTML = `<h1>${item.name}</h1><h6>Manufacturer:</h6><p>${item.manufacturer}</p><h6>Model:</h6><p>${item.model}</p><h6>Vehicle class:</h6><p>${item.vehicle_class}</p>`;
+      } else if (item.starship_class) {
+        card.innerHTML = `<h1>${item.name}</h1><h6>Manufacturer:</h6><p> ${item.manufacturer}</p><h6>Model:</h6><p>${item.model}</p><h6>Starship class:</h6><p>${item.starship_class}</p>`;
+      } else if (item.title) {
+        card.innerHTML = `<h1>${item.title}</h1><p>${item.opening_crawl}</p>`;
       }
     });
   });
@@ -95,10 +127,14 @@ function getInfo() {
 
 function doSomething(value) {
   btnValue = value;
+  
   clearData();
   getInfo();
 }
-
+function getImageDirectoryByFullURL(nextUrl) {
+  nextUrl = nextUrl.split("/");
+  nextUrl = nextUrl.pop();
+}
 function clearData() {
   let clear = document.getElementById("data");
   clear.innerHTML = "";

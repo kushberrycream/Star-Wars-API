261369d39926e7470520458fe5c2a8dda50d40c4
kushberrycream <tom@wilson-express.co.uk>
2020-03-29 21:25:00 +0000

    styling and refresh btn added

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,8 @@
 const app = document.getElementById("root");
-const button = document.getElementsByClassName("btn");
-const nav = document.createElement("div");
-nav.setAttribute("class", "container api-selectors appear");
+const logo = document.createElement("img");
+logo.src = "assets/starwars.png";
+logo.setAttribute("class", "img single-featured-image-header ");
+logo.setAttribute("alt", "StarWars Logo");
 
 const container = document.createElement("div");
 container.setAttribute("class", "container-fluid cards");
@@ -10,66 +11,64 @@ const row = document.createElement("div");
 row.setAttribute("class", "row justify-content-center");
 row.setAttribute("id", "data");
 
-const logo = document.createElement("img");
-logo.src = "assets/starwars.png";
-logo.setAttribute("class", "img-fluid single-featured-image-header");
-logo.setAttribute("alt", "StarWars Logo");
+const refresh = document.createElement("div");
+refresh.setAttribute("class", "refresh-btn");
+refresh.setAttribute("onclick", "window.location.reload()");
+refresh.innerHTML = `<i class="fas fa-redo"></i>`;
 
 const apiUrl = `https://swapi.co/api/`;
 
-const peopleBtn = document.createElement("button");
-peopleBtn.setAttribute("class", "btn btn-warning");
+const peopleBtn = document.createElement("div");
+peopleBtn.setAttribute("class", "cardnav character col-lg-3");
 peopleBtn.setAttribute("onclick", "doSomething(value) ");
-peopleBtn.innerText = "People";
+peopleBtn.innerHTML = `<p class="btn-title">Characters</p>`;
 peopleBtn.value = "people";
 
-const planetsBtn = document.createElement("button");
-planetsBtn.setAttribute("class", "btn btn-warning");
+const planetsBtn = document.createElement("div");
+planetsBtn.setAttribute("class", "cardnav planets col-lg-3");
 planetsBtn.setAttribute("onclick", "doSomething(value) ");
-planetsBtn.innerText = "Planets";
+planetsBtn.innerHTML = `<p class="btn-title">Planets</p>`;
 planetsBtn.value = "planets";
 
-const speciesBtn = document.createElement("button");
-speciesBtn.setAttribute("class", "btn btn-warning");
+const speciesBtn = document.createElement("div");
+speciesBtn.setAttribute("class", "cardnav species col-lg-3");
 speciesBtn.setAttribute("onclick", "doSomething(value)");
-speciesBtn.innerText = "Species";
+speciesBtn.innerHTML = `<p class="btn-title">Species</p>`;
 speciesBtn.value = "species";
 
-const vehiclesBtn = document.createElement("button");
-vehiclesBtn.setAttribute("class", "btn btn-warning");
+const vehiclesBtn = document.createElement("div");
+vehiclesBtn.setAttribute("class", "cardnav vehicles col-lg-3");
 vehiclesBtn.setAttribute("onclick", "doSomething(value)");
-vehiclesBtn.innerText = "Vehicles";
+vehiclesBtn.innerHTML = `<p class="btn-title">vehicles</p>`;
 vehiclesBtn.value = "vehicles";
 
-const starshipsBtn = document.createElement("button");
-starshipsBtn.setAttribute("class", "btn btn-warning");
+const starshipsBtn = document.createElement("div");
+starshipsBtn.setAttribute("class", "cardnav starships col-lg-3");
 starshipsBtn.setAttribute("onclick", "doSomething(value)");
-starshipsBtn.innerText = "Starships";
+starshipsBtn.innerHTML = `<p class="btn-title">Starships</p>`;
 starshipsBtn.value = "starships";
 
-const filmsBtn = document.createElement("button");
-filmsBtn.setAttribute("class", "btn btn-warning");
+const filmsBtn = document.createElement("div");
+filmsBtn.setAttribute("class", "cardnav films col-lg-3");
 filmsBtn.setAttribute("onclick", "doSomething(value)");
-filmsBtn.innerText = "Films";
+filmsBtn.innerHTML = `<p class="btn-title">Films</p>`;
 filmsBtn.value = "films";
 
 app.appendChild(logo);
-app.appendChild(nav);
-nav.appendChild(peopleBtn);
-nav.appendChild(planetsBtn);
-nav.appendChild(speciesBtn);
-nav.appendChild(vehiclesBtn);
-nav.appendChild(starshipsBtn);
-nav.appendChild(filmsBtn);
 app.appendChild(container);
 container.appendChild(row);
+row.appendChild(peopleBtn);
+row.appendChild(planetsBtn);
+row.appendChild(speciesBtn);
+row.appendChild(vehiclesBtn);
+row.appendChild(starshipsBtn);
+row.appendChild(filmsBtn);
 
 let btnValue = {};
-let nextPage = {};
-let prevPage = {};
 
 function getInfo() {
   axios.get(apiUrl + btnValue).then(response => {
+    app.appendChild(refresh);
     this.data = response.data;
     let pagination = "";
     if (data.next || data.previous) {
@@ -82,7 +81,8 @@ function getInfo() {
       card.setAttribute("class", "card col-lg-3");
 
       if (item.height) {
-        card.innerHTML = `<h1>${item.name}</h1><h6>Height:</h6><p> ${item.height} cm</p><h6>Hair color:</h6><p>${item.hair_color}</p><h6>Birth Year:</h6><p>${item.birth_year}</p>`;
+        let bby = item.birth_year.split("BBY").join(" BBY");
+        card.innerHTML = `<h1>${item.name}</h1><h6>Height:</h6><p> ${item.height} cm</p><h6>Hair color:</h6><p>${item.hair_color}</p><h6>Birth Year:</h6><p>${bby}</p>`;
       } else if (item.terrain) {
         card.innerHTML = `<h1>${item.name}</h1><h6>Terrain:</h6><p> ${item.terrain}</p><h6>Gravity:</h6><p>${item.gravity}</p><h6>Climate:</h6><p>${item.climate}</p>`;
       } else if (item.classification) {
@@ -100,14 +100,10 @@ function getInfo() {
 
 function doSomething(value) {
   btnValue = value;
-
   clearData();
   getInfo();
 }
-function getImageDirectoryByFullURL(nextUrl) {
-  nextUrl = nextUrl.split("/");
-  nextUrl = nextUrl.pop();
-}
+
 function clearData() {
   let clear = document.getElementById("data");
   clear.innerHTML = "";
